00168b1582a9612cf6620f3f0139c36089abea1a
Mariyan <marian.sokolov1@gmail.com>
2025-03-10 20:07:19 +0200

    category deletion made

--- src/schema/mutations.ts
+++ src/schema/mutations.ts
@@ -1,7 +1,7 @@
 import { GraphQLObjectType } from 'graphql';
 import { createWallet, deleteWallet, updateWallet } from './resolvers/walletResolvers';
 import { createUser, loginUser } from './resolvers/userResolvers';
-import { createCategory } from './resolvers/categoryResolvers';
+import { createCategory, deleteCategory } from './resolvers/categoryResolvers';
 import { createTransaction, deleteTransaction, updateTransaction } from './resolvers/transactionResolvers';
 
 export const Mutation = new GraphQLObjectType({
@@ -13,6 +13,7 @@ export const Mutation = new GraphQLObjectType({
     updateWallet,
     deleteWallet,
     createCategory,
+    deleteCategory,
     createTransaction,
     updateTransaction,
     deleteTransaction
--- src/schema/resolvers/categoryResolvers.ts
+++ src/schema/resolvers/categoryResolvers.ts
@@ -22,3 +22,26 @@ export const createCategory: GraphQLFieldConfig<any, any, { [argName: string]: a
     }
   },
 };
+
+export const deleteCategory: GraphQLFieldConfig<any, any, { [argName: string]: any }> = {
+  type: CategoryType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+  },
+  resolve: async (parent, args, context) => {
+    try {
+      await authenticateUser(context.request.headers.authorization);
+      const category = await Category.findByPk(args.id);
+
+      if (!category) {
+        throw new Error('Category not found');
+      }
+
+      category.deletedAt = new Date();
+      await category.save();
+      return category;
+    } catch (error) {
+      throw new Error(`Error deleting category: ${error.message}`);
+    }
+  },
+};
