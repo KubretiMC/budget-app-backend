2411615ff12fae33f0393106139f2fdde298bca8
Mariyan <marian.sokolov1@gmail.com>
2025-04-02 03:11:02 +0300

    env fixes

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
+  process.env.MYSQL_ADDON_DB as string,
+  process.env.MYSQL_ADDON_USER as string,
+  process.env.MYSQL_ADDON_PASSWORD as string,
   {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    host: process.env.MYSQL_ADDON_HOST,
+    port: Number(process.env.MYSQL_ADDON_PORT),
     dialect: process.env.DB_DIALECT as Dialect,
     logging: false,
   }
